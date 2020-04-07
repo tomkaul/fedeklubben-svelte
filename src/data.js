5eb78bb91a4f3ce04348a72bd65bbf985e5eaa72
Thomas Kaulberg (TomKaul) <thomas.kaulberg@gmail.com>
2020-04-07 08:33:44 +0200

    The weekly update

--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@ export default {
     { x: 12, y: -0.5 },
     { x: 13, y: -1.0 },
     { x: 14, y: -1.5 },
+    { x: 15, y: -4.4 },
   ]},
   'Jacob': {start_weight: 88.7,
     data: [
@@ -13,6 +14,7 @@ export default {
     { x: 12, y: -1.4 },
     { x: 13, y: -2.0 },
     { x: 14, y: -3.0 },
+    { x: 15, y: -2.4 },
   ]},
   'Manse': {start_weight: 108.2,
     data: [
@@ -27,5 +29,6 @@ export default {
     { x: 12, y: -1.4 },
     { x: 13, y: -0.8 },
     { x: 14, y: -1.1 },
+    { x: 15, y: -1.8 },
   ]},
 };
